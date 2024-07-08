49a96fc30a035b1c1309dc5cabbe7e27aef8017b
gte <te.gerard@yahoo.fr>
2024-07-09 00:47:06 +0100

    More change

--- bin/types.ml
+++ bin/types.ml
@@ -1,1 +1,7 @@
 type connection_status = Connected | Disconnected
+
+type message_payload = { from: string; body: string }
+
+type message =
+     | SEND of message_payload
+     | ACK of message_payload
