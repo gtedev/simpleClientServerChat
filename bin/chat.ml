089a9e6712e8d0a542499b5f03d38dbfede47a2b
gte <te.gerard@yahoo.fr>
2024-07-09 20:06:04 +0100

    Some more change

--- bin/chat.ml
+++ bin/chat.ml
@@ -14,12 +14,31 @@ module ServerConfig = struct
     (socket, sockaddr)
 end
 
+module Message = struct
+  type message_payload = { from : string; body : string }
+  type t = SEND of message_payload | ACK of message_payload
+
+  let pipe = String.concat "|"
+    
+  let toString message =
+    match message with
+    | SEND payload -> ["SEND"; payload.from; payload.body] |> pipe
+    | ACK payload -> ["ACK"; payload.from; payload.body] |> pipe
+end
+
+
+
 open  ServerConfig
+
+let send client_sock message = 
+  let bytes = Bytes.of_string (message ^ "\n") in
+  let length = Bytes.length bytes in
+  Lwt_unix.send client_sock bytes 0 length []
 
 let rec receive_messages client_sock =
   let buffer = Bytes.create buffer_size in
   Lwt_unix.recv client_sock buffer 0 buffer_size [] >>= fun bytes_read ->
-  if bytes_read = 0 then printl "Server closed the connection."
+  if bytes_read = 0 then printl "Connection closed..."
   else
     let message = Bytes.sub_string buffer 0 bytes_read in
     printl ("Received: " ^ message) >>= fun () -> receive_messages client_sock
@@ -27,8 +46,7 @@ let rec receive_messages client_sock =
 let rec send_messages client_sock =
   Lwt_io.read_line_opt stdin >>= function
   | Some message ->
-      let message_bytes = Bytes.of_string (message ^ "\n") in
-      Lwt_unix.send client_sock message_bytes 0 (Bytes.length message_bytes) []
+      send client_sock message
       >>= fun _ -> send_messages client_sock
   | None -> Lwt_unix.close client_sock
 
