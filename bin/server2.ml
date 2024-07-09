4f008268f4169d95dcae240153112816187f0793
gte <te.gerard@yahoo.fr>
2024-07-09 18:20:59 +0100

    More changes

--- bin/server2.ml
+++ bin/server2.ml
@@ -1,29 +1,31 @@
+open Lwt
 open Lwt.Infix
 open Util2
 open Lwt_unix
 
-let wait_incoming_connections  server_sock = 
-  fun () ->
-    (* Function to accept client connections and handle them *)
-    let rec accept_connections () =
-      Lwt_unix.accept server_sock >>= fun (client_sock, _) ->
-      Lwt.async (fun () ->
-        Lwt_io.printf "Client connected.\n" >>=  start_chat client_sock 
-      );
-      accept_connections ()
-    in
-  
+let wait_incoming_connections server_sock () =
+  (* Function to accept client connections and handle them *)
+  let rec accept_connections () =
+    accept server_sock >>= fun (client_sock, _) ->
+    async (fun () ->
+
+        log_console "Client connected.\n" ()
+         >>= start_chat client_sock);
+
     accept_connections ()
+  in
+
+  accept_connections ()
 
 (* Main server function *)
 let main () =
-  let server_sock, _ = get_server_socket_config () in
-
+  let server_sock, sockaddr = get_server_socket_config () in
   setsockopt server_sock SO_REUSEADDR true;
-  listen server_sock 10;
 
-  log_console  "Server listening on port 8080..." ()
+  bind server_sock sockaddr >>= fun () ->
+  listen server_sock 1;
+
+  log_console "Server listening on port 9000..." ()
   >>= wait_incoming_connections server_sock
 
-let initiate() =
-  Lwt_main.run (main ())
+let initiate () = Lwt_main.run (main ())
