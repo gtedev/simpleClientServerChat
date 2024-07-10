4258cf150981246fe4a40775ff6e35e096e49639
gte <te.gerard@yahoo.fr>
2024-07-10 04:00:16 +0100

    Update message.ml

--- bin/message.ml
+++ bin/message.ml
@@ -1,30 +1,28 @@
 type message_payload = {
   from : string;
   body : string;
-  timestamp_sent : float option;
+  timestamp : float option;
 }
 
 type t = SEND of message_payload | ACK of message_payload
 
 let separator = '|'
 let join_with_pipe = String.concat (separator |> String.make 1)
-let create_send from timestamp_sent body = SEND { from; body; timestamp_sent }
-let create_ack from timestamp_sent body = ACK { from; body; timestamp_sent }
+let create_send from timestamp body = SEND { from; body; timestamp }
+let create_ack from timestamp body = ACK { from; body; timestamp }
 
 module Float = struct
   let to_string_or_default (value : float option) =
-    value
-    |> Option.map string_of_float 
-    |> Option.value ~default:""
+    value |> Option.map string_of_float |> Option.value ~default:""
 end
 
 let toString message =
   match message with
-  | SEND { from; body; timestamp_sent } ->
-      [ "SEND"; from; body; Float.to_string_or_default timestamp_sent ]
+  | SEND { from; body; timestamp } ->
+      [ "SEND"; from; body; Float.to_string_or_default timestamp ]
       |> join_with_pipe
-  | ACK { from; body; timestamp_sent } ->
-      [ "ACK"; from; body; Float.to_string_or_default timestamp_sent ]
+  | ACK { from; body; timestamp } ->
+      [ "ACK"; from; body; Float.to_string_or_default timestamp ]
       |> join_with_pipe
 
 let toPayload messageString =
@@ -32,11 +30,8 @@ let toPayload messageString =
     String.split_on_char separator messageString |> List.map Base.String.strip
   in
   match params with
-  | [ "SEND"; from; body; timestamp_sent ] ->
-      Some
-        (SEND
-           { from; body; timestamp_sent = float_of_string_opt timestamp_sent })
-  | [ "ACK"; from; body; timestamp_sent ] ->
-      Some
-        (ACK { from; body; timestamp_sent = float_of_string_opt timestamp_sent })
+  | [ "SEND"; from; body; timestamp ] ->
+      Some (SEND { from; body; timestamp = float_of_string_opt timestamp })
+  | [ "ACK"; from; body; timestamp ] ->
+      Some (ACK { from; body; timestamp = float_of_string_opt timestamp })
   | _ -> None
